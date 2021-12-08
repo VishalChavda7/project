53b3314337e2198578686c961f9bae77d18bc770
VishalChavda7 <vishalchavda7222@gmail.com>
2021-12-08 16:20:09 +0530

    project

--- app/src/main/java/com/vishalchavda/project/MainActivity.java
+++ app/src/main/java/com/vishalchavda/project/MainActivity.java
@@ -6,11 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText etone,ettwo,etthree;
+    TextView etone, ettwo, etthree;
     Button next;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,28 +28,39 @@ public class MainActivity extends AppCompatActivity {
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (etthree.getText().toString().matches("9")) {
+
+                int c = Integer.parseInt(etthree.getText().toString());
+                int a;
+                int b ;
+
+                a = c + 1;
+                b = a + 1;
+                c = b + 1;
+
+                if (a == 10) {
+                    b = 1;
+                    c = b + 1;
+                }
+                if (b == 10) {
+                    c = 1;
+                }
+
+                if (etthree.getText().toString().matches("10")) {
                     etone.setText("1");
                     ettwo.setText("2");
                     etthree.setText("3");
                     return;
                 }
-                    settext();
+                etone.setText(String.valueOf(a));
+                ettwo.setText(String.valueOf(b));
+                etthree.setText(String.valueOf(c));
 
 
 
 
             }
         });
-    }
-    private void settext(){
 
-        int a = Integer.parseInt(etone.getText().toString());
-        int b = Integer.parseInt(ettwo.getText().toString());
-        int c = Integer.parseInt(etthree.getText().toString());
-
-        etone.setText(String.valueOf(a+3));
-        ettwo.setText(String.valueOf(b+3));
-        etthree.setText(String.valueOf(c+3));
     }
+
 }
